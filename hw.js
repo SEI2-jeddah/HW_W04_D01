3c0be607d1a4efc2afd54d69851bbcd8e3f2708c
MSalah73 <ms24@pdx.edu>
2019-10-20 23:10:34 +0300

    Mohammed Salah

--- hw.js
+++ hw.js
@@ -1,7 +1,3 @@
-​// ------ FOR ALL THE EXCERCISES ONLY USE ARROW FUNCTIONS  ----- //
-​
-
-
 // PART 1:  USE MAP IN THE FOLLOWING EXCERCISES //
 
 // A) write a function called swapCase that takes a string of words and uses .map and your newly written capitalize()
@@ -9,17 +5,19 @@
 // Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
 // ex: swapCase('hello stranger , what do we have today? ') // => " HELLO stranger , WHAT do WE have TODAY ?"
 var swapCase = function(string) {
-  // Codeeeee
+	return string.split(" ").map((valueAtIndex, index) => index % 2 === 0? valueAtIndex.toUpperCase(): valueAtIndex).join(" ")
 };
-​
+console.log(swapCase('hello stranger, what do we have today? '))
 // B) Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
 // alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.charCodeAt() .
 // see if you can use Ascii code to acomplish this
 // ex. shiftLetters('hello') // => 'ifmmp'
 // ex. (shiftLetters('abcxyz') // => "bcdyz{"
 var shiftLetters = function(string) {
-  // code!
+	return string.split("").map(char => String.fromCharCode(char.charCodeAt(0)+1)).join("")
 };
+console.log(shiftLetters("hello"));
+
 
 
 
@@ -46,14 +44,30 @@ var shiftLetters = function(string) {
 // - ace of diamonds
 // - 2 of diamonds
 // - 3 of diamonds 
-
-
+let deck = ["clubs", "diamonds", "hearts","spades"];
+deck.forEach((element, index) =>{ 
+		let suit = ["ace",2,3,4,5,6,7,8,9,10,"jack","queen", "king"]
+		suit.forEach((element2, index2, arr) => arr[index2] = element2 + " of "+ element)
+		deck[index] = suit
+	})
+deck.forEach(x => x.forEach((card => console.log(card))))
 // B) Word Play
 // Create a form where users may enter a sentence.
 // Turn that sentence into an array using the split method.
 // Then, loop through this array to build a new array out of every word in the sentence that is 3 or more characters in length.
 // Finally, reverse the order of the new array, join it back together into a string, and display it to the user.
-
-
-
-
+function wordPlay(){
+	let sentence = document.querySelector("#input").value //"hello hellp not ah oh hey not this again az za hi";
+	let newArr = []
+	sentence.split(" ").forEach((element, index, arr) => element.length >= 3? newArr.push(element.split("").reverse().join("")): newArr)
+	let toAdd = document.querySelector("#addHere");
+	toAdd.innerText = "Original sentence: "+sentence+"\nWord Play: " + newArr.join(" ");
+}
+function wordPlayCMD(){
+	let sentence = "hello hellp not ah oh hey not this again az za hi";
+	console.log("Original sentence: "+sentence);
+	let newArr = []
+	sentence.split(" ").forEach((element, index, arr) => element.length >= 3? newArr.push(element.split("").reverse().join("")): newArr)
+	console.log("Word Play: "+newArr.join(" "));
+}
+wordPlayCMD();
