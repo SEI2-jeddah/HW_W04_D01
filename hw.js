dc8b3c2a63d002df2b8c835d7df074b8a182b80a
Israa Shuri <esraa-shori@hotmail.com>
2019-10-25 22:54:24 +0300

    Israa Shori HW

--- hw.js
+++ hw.js
@@ -1,27 +1,39 @@
-​// ------ FOR ALL THE EXCERCISES ONLY USE ARROW FUNCTIONS  ----- //
-​
-
-
-// PART 1:  USE MAP IN THE FOLLOWING EXCERCISES //
 
 // A) write a function called swapCase that takes a string of words and uses .map and your newly written capitalize()
 // function to return a string where every other word is in all caps.
 // Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
 // ex: swapCase('hello stranger , what do we have today? ') // => " HELLO stranger , WHAT do WE have TODAY ?"
-var swapCase = function(string) {
-  // Codeeeee
-};
-​
+// var swapCase = function(string) {
+//   // Codeeeee
+// };
+
+var swapCase = (string) => {
+  string = string.split(' ')
+  string = string.map(function(word,i){
+    if ( i % 2 == 0) {  
+      word = word.toUpperCase()
+     }
+      return word   
+  })
+  console.log( string.join(' ') )
+}
+swapCase('hello stranger, what do we have today?')
+// ​
 // B) Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
 // alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.charCodeAt() .
 // see if you can use Ascii code to acomplish this
 // ex. shiftLetters('hello') // => 'ifmmp'
 // ex. (shiftLetters('abcxyz') // => "bcdyz{"
-var shiftLetters = function(string) {
-  // code!
-};
+// var shiftLetters = function(string) {
+//   // code!
+// };
 
-
+var shiftLetters = (string) => {
+ 
+  return string.split('').map((element) => String.fromCharCode(element.charCodeAt()+1)).join('');
+}
+console.log(shiftLetters('hello'))
+console.log(shiftLetters('abcxyz'))
 
 
 // PART 2: USE FOREACH IN THE FOLLOWING EXCERCISES
@@ -46,7 +58,15 @@ var shiftLetters = function(string) {
 // - ace of diamonds
 // - 2 of diamonds
 // - 3 of diamonds 
-
+var suits = ["clubs","diamonds","hearts","spades"];
+var ranks = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+suits.forEach(suitse => {
+    ranks.forEach(rankse => {
+        console.log(rankse+" of "+suitse);
+        
+    });
+    
+});
 
 // B) Word Play
 // Create a form where users may enter a sentence.
@@ -54,6 +74,14 @@ var shiftLetters = function(string) {
 // Then, loop through this array to build a new array out of every word in the sentence that is 3 or more characters in length.
 // Finally, reverse the order of the new array, join it back together into a string, and display it to the user.
 
-
+var wordPlay = (str) =>{
+  var arrayNew = str.split(' ')
+  var arrayTmp = [];
+  arrayNew.forEach(element => {if(element.length >2){arrayTmp.push(element)}});
+   return console.log(arrayTmp.reverse().join(' ')); 
+  }
+  var str = "Sorry for late homeWork Submit but I forget my charger in makkah "
+  
+  wordPlay(str)
 
 
