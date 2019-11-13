f69d7cd9606c839d50c37fa2adcdfba9aec89b79
Tareq Alotaibi <tareqdev@protonmail.com>
2019-11-13 17:34:30 +0300

    Finished homework

--- hw.js
+++ hw.js
@@ -1,16 +1,4 @@
-​// ------ FOR ALL THE EXCERCISES ONLY USE ARROW FUNCTIONS  ----- //
-​
 
-
-// PART 1:  USE MAP IN THE FOLLOWING EXCERCISES //
-
-// A) write a function called swapCase that takes a string of words and uses .map and your newly written capitalize()
-// function to return a string where every other word is in all caps.
-// Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take.
-// ex: swapCase('hello stranger , what do we have today? ') // => " HELLO stranger , WHAT do WE have TODAY ?"
-var swapCase = function(string) {
-  // Codeeeee
-};
 ​
 // B) Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the
 // alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.charCodeAt() .
@@ -31,21 +19,21 @@ var shiftLetters = function(string) {
 // Then, display a list of every card in the deck. (Hint: Each element of the array should read something like "ace of spades" or "4 of hearts").
 // The start of your output should look something like this:
 // - ace of clubs
-// - 2 of clubs 
-// - 3 of clubs 
-// - 4 of clubs 
-// - 5 of clubs 
-// - 6 of clubs 
-// - 7 of clubs 
-// - 8 of clubs 
-// - 9 of clubs 
-// - 10 of clubs 
-// - jack of clubs 
-// - queen of clubs 
-// - king of clubs 
+// - 2 of clubs
+// - 3 of clubs
+// - 4 of clubs
+// - 5 of clubs
+// - 6 of clubs
+// - 7 of clubs
+// - 8 of clubs
+// - 9 of clubs
+// - 10 of clubs
+// - jack of clubs
+// - queen of clubs
+// - king of clubs
 // - ace of diamonds
 // - 2 of diamonds
-// - 3 of diamonds 
+// - 3 of diamonds
 
 
 // B) Word Play
@@ -53,7 +41,3 @@ var shiftLetters = function(string) {
 // Turn that sentence into an array using the split method.
 // Then, loop through this array to build a new array out of every word in the sentence that is 3 or more characters in length.
 // Finally, reverse the order of the new array, join it back together into a string, and display it to the user.
-
-
-
-
---
+++ solution.js
@@ -0,0 +1,51 @@
+// First question
+// var swapCase = function(string) {
+//
+//
+//           var array = string.split(" ");
+//
+//
+//
+//           var array = array.map((item , index ) => {
+//
+//                     if(index % 2 == 0){
+//                               item = item.toUpperCase();
+//                     }
+//
+//                     return item
+//
+//
+//           })
+//           let answer = ''
+//           for( element of array){
+//                     answer += " " + element
+//           }
+//
+//           console.log(answer);
+//
+// };
+//
+// swapCase("Hello there I'm glad we've got the chance to meet again ")
+
+
+//Second question
+
+
+
+// fourth Question
+let rev = (string) => {
+
+let split = string.split("")
+let reverse = split.reverse()
+let lastOne = split.join("")
+console.log(lastOne);
+
+
+}
+
+
+
+
+
+
+rev("tareq")
